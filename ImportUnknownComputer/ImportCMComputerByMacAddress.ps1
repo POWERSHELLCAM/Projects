4f45e3029a18a33eae22a06c1b1d57754899bda7
SHISHIR KUSHAWAHA <srktcg@gmail.com>
2022-11-24 14:57:02 +0530

    Update ImportCMComputerByMacAddress.ps1

--- ImportUnknownComputer/ImportCMComputerByMacAddress.ps1
+++ ImportUnknownComputer/ImportCMComputerByMacAddress.ps1
@@ -1,36 +1,7 @@
 
 $init=
 {
-    function sendmail($message,$techemail)
-    {
-        $TextInfo = (Get-Culture).TextInfo
-        $Mailbox = ""
-        $SMTP = ""  
-        $Subject = "Device $LocalComputername imported in MECM "
-        $body = "<HTML><HEAD></HEAD>"
-        $body+='<BODY style="font-size:11pt;font-family:Calibri;color:black">'
-        $body += "<br>"
-        $body += "Hello $($TextInfo.ToTitleCase($username)),<br>"
-        $body += "<br>"
-        $body += $message
-        $body += "</body>"
-        
-        #region send Windows Deployment Email with additional information
-        $params = $null
-        $params = @{ 
-            To = $techemail
-            Subject = $Subject
-            Body = $body 
-            SmtpServer = $SMTP
-            From = $Mailbox
-            BodyAsHtml = $true 
-        } 
-        Send-MailMessage @params -ErrorAction SilentlyContinue
-        writemsg "Mail send status : $?"
-        #endregion send Windows Deployment Email with additional information
-
-    }
-
+    
     function writemsg($m)
     {
         Write-Output "$m ($(get-date -format G))" | Out-File $importlog -Append
@@ -109,7 +80,7 @@ $init=
             writemsg "Device $LocalComputername having resource id $resourceid failed to add in image deployment group. $?"
             writemsg $_
             return $false
-            $global:mailstatus="failed"
+            $global:mailstatus="Failed"
         }
     }
     function importDeviceToSCCM ($LocalComputerName,$id)
@@ -144,7 +115,7 @@ $init=
         catch 
         {
             writemsg $_
-            $global:mailstatus="failed"
+            $global:mailstatus="Failed"
         }
     }
     function requestImport($techemail,$uniqueid,$hostname,$collectionid,$user,$csvfilepath)
@@ -154,14 +125,17 @@ $init=
         $global:resourceid=$null
         $LocalComputerName=$hostname
         $username=$user
+        $extramsg=$null
         $global:addedtoimagecollection=$false
-        $SCCMServerName='<sccm primary>'
-        $SCCMNameSpace='root\sms\site_MP1'
+        $SCCMServerName=''
+        $SCCMNameSpace='root\sms\site_'
         $sharedpath="c:\temp\ImportUnknownComputer"
         $importlog="$sharedpath\logs\$hostname.log"
         writemsg "CollectionId: $collectionid"
         writemsg "User Name : $username"
         $multiplerecords=$false
+        $comment=""
+        $collectionname=(Get-WMIObject -Computer $SCCMServerName -Namespace $SCCMNameSpace -class SMS_Collection -filter "CollectionID='$collectionId'").name
         if($uniqueid.length -eq 17)
         {
             $existingdevice=Get-WmiObject -class "SMS_R_System" -namespace $SCCMNameSpace -Filter "MACAddresses = '$($uniqueid)'" -ComputerName $SCCMServerName
@@ -186,32 +160,38 @@ $init=
                 {
                     if($addedtoimagecollection)
                     {
-                        $global:mailstatus="completed"
-                        $global:mailmessage="Your request to add device <b><i>$hostname</i></b> having Unique ID <b><i>$uniqueid</i></b> is processed successfully. You may now proceed with the image deployment."
+                        $global:mailstatus="Completed"
+                        $global:mailmessage="<p style='color:green'>Registered. Proceed with imaging.</p>"
                     }
                     else 
                     {
                         $global:mailstatus="Failed"
-                        $global:mailmessage="Your request to add device <b><i>$hostname</i></b> having Unique ID <b><i>$uniqueid</i></b> is failed. Please connect SOE Team for solution."
+                        $global:mailmessage="<p style='color:red'>Failed to register. Connect GT MECM Platform Team for issue and solution.</p>"
                     }
                 }
             }
             catch 
             {
                 writemsg $_
-                $global:mailstatus="failed"
-                $global:mailmessage="Your request to add device <b><i>$hostname</i></b> having Unique ID <b><i>$uniqueid</i></b> is failed. Please connect SOE Team for solution."
+                $global:mailstatus="Failed"
+                $global:mailmessage="<p style='color:red'>Failed to register. Connect GT MECM Platform Team for issue and solution.</p>"
+                $comment="Failed to create record in MECM."
             } 
         }
         else 
         {
-            writemsg "A device with name $hostname already exist with UniqueID $uniqueid"
-            $extramsg=$null
+            $existingname=$($existingdevice.name)
+            if($existingname -ne $hostname)
+            {
+                $comment="Existing device name : $existingname"
+            }
+            writemsg "A device with name $existingname already exist with UniqueID $uniqueid"
             if($multiplerecords)
             {
                 $global:resourceid=$($existingdevice[0].resourceid)
-                $extramsg="**Duplicate records found with UniqueID $uniqueid. Please forward this mail to SOE engineer.**"
+                $extramsg="**Duplicate records found with UniqueID $uniqueid.**"
                 writemsg $extramsg
+                $comment=$extramsg
             }
             else 
             {
@@ -220,41 +200,104 @@ $init=
             
             if(isPartOfImageCollection)
             {
-                $global:mailstatus="completed"
+                $global:mailstatus="Completed"
                 writemsg "A device with name $hostname already part of imaging collection $collectionid."
-                $global:mailmessage="Your requested device <b><i>$hostname</i></b> having Unique ID <b><i>$uniqueid</i></b> is already registered with MECM. You may now proceed with the image deployment.<br><p style='color:red'>$extramsg</p>"
+                $global:mailmessage="<p style='color:green'>Already Registered. Proceed with imaging.</p>"
             }
             else 
             {
                 addDeviceToSCCMCollection
                 if($addedtoimagecollection)
                 {
-                    $global:mailstatus="completed"
-                    $global:mailmessage="Your requested device <b><i>$hostname</i></b> having Unique ID <b><i>$uniqueid</i></b> is already registered with MECM. The requates is processed successfully. You may now proceed with the image deployment.<br><p style='color:red'>$extramsg</p>"
+                    $global:mailstatus="Completed"
+                    $global:mailmessage="<p style='color:green'>Already Registered. Proceed with imaging.</p>"
                 }
                 else 
                 {
-                    $global:mailstatus="failed"
-                    $global:mailmessage="Your request to add device <b><i>$hostname</i></b> having Unique ID <b><i>$uniqueid</i></b> is failed. Please connect SOE Team for solution.<br><p style='color:red'>$extramsg</p>"
+                    $global:mailstatus="Failed"
+                    $global:mailmessage="<p style='color:red'>Failed to register. Connect GT MECM Platform Team for issue and solution.</p>"
+                    $comment="Failed to add in imaging collection."
                 }
             }
         }
         if($mailmessage -eq "")
         {
-            $global:mailmessage="Your request to add device <b><i>$hostname</i></b> having Unique ID <b><i>$uniqueid</i></b> is failed. Please connect SOE Team for solution.<br><p style='color:red'>$extramsg</p>"
+            $global:mailmessage="<p style='color:red'>Failed to register .Connect GT MECM Platform Team for issue and solution.</p>"
         }
-        sendmail $mailmessage $techemail
+        #sendmail $mailmessage $techemail
         writemsg "------------------------------------------------------------------------------------------------------------------------------------"
         if($mailstatus -ne "completed")
         {
-            return $csvfilepath
+            #return $csvfilepath
         }
+        $myReport=$null
+        $myReport = [pscustomobject]@{
+            ComputerName = $hostname
+            Username=$username
+            ResourceID = $resourceid
+            UniqueID = $uniqueid
+            CollectionID=$collectionid
+            CollectionName=$collectionname
+            Status    = $mailstatus
+            Email= $techemail
+            Message=$mailmessage
+            Comment=$comment
+            CSVFile=$csvfilepath
+        }
+        return $myReport
     }
 }
+
+function sendmail($techemail,$username,$table)
+    {
+        $TextInfo = (Get-Culture).TextInfo
+        $Mailbox = ""
+        $SMTP = ""  
+        $Subject = "Your device import status in MECM "
+        $body = "<HTML><HEAD></HEAD>"
+        $body+='<BODY style="font-size:11pt;font-family:Calibri;color:black">'
+        $body += "<br>"
+        $body += "Hello $($TextInfo.ToTitleCase($username)),<br>"
+        $body += "<br>"
+        $body += "Thanks for your request. Please find the import status of device/s requested."
+        $body += "<br>"
+        $body += "$table"
+        $body += "<br>"
+        $body += "Regards,<br>GT MECM Platform Team"
+        $body += "</body>"
+        
+        #region send Windows Deployment Email with additional information
+        $params = $null
+        $params = @{ 
+            To = $techemail
+            Subject = $Subject
+            Body = $body 
+            SmtpServer = $SMTP
+            From = $Mailbox
+            BodyAsHtml = $true 
+        } 
+        Send-MailMessage @params -ErrorAction SilentlyContinue
+        writemsg "Mail send status : $?"
+        #endregion send Windows Deployment Email with additional information
+
+    }
+
 function writemsg($m)
 {
     Write-Output "$m ($(get-date -format G))" | Out-File $importlog -Append
 }
+#--CSS formatting
+$test=@'
+<style type="text/css">
+ h1, h5,h2, th { text-align: left; font-family: Segoe UI;font-size: 13px;}
+table { margin: left; font-family: Segoe UI; box-shadow: 10px 10px 5px #888; border: thin ridge grey; }
+th { background: #0046c3; color: #fff; max-width: 400px; padding: 5px 10px; font-size: 12px;}
+td { font-size: 11px; padding: 5px 20px; color: #000; }
+tr { background: #b8d1f3; }
+tr:nth-child(even) { background: #dae5f4; }
+tr:nth-child(odd) { background: #b8d1f3; }
+</style>
+'@
 $global:mailstatus=$null
 $global:mailmessage=""
 $global:resourceid=$null
@@ -269,6 +312,8 @@ Get-ChildItem -Path "$sharedpath\logs\*.log" | Where-Object {($_.LastWriteTime -
 $csvdumps="$sharedpath\PSCode\csvdumps"
 $collectionlist=Import-Csv -Path "$sharedpath\db\coutryCollList.csv"
 $csvlist=Get-ChildItem -Path $csvdumps -Filter *.csv #| Where-Object {$_.LastWriteTime -gt (Get-Date).AddDays(-2) } | Sort-Object LastWriteTime -Descending
+$finalreport=@()
+
 
 if($null -ne $csvlist)
 {
@@ -287,14 +332,18 @@ if($null -ne $csvlist)
                 $global:mailstatus=""
                 $collectionid=($collectionlist -match $($csvimported.country)).collectionid
                 $username=$($csvimported.email).split('.')[0]
-                writemsg "A thread initiated for processing $($csvimported.MACAddress)"
-                if($($csvimported.MACAddress))
-                {
-                    Start-Job -ScriptBlock {requestImport $args[0] $args[1] $args[2] $args[3] $args[4] $args[5]} -InitializationScript $init -ArgumentList ($($csvimported.email), $($csvimported.MACAddress), $($csvimported.computer),$collectionid,$username,$($list.fullname))
-                }
+                
+                $requested=$false
                 if($($csvimported.guid))
                 {
+                    writemsg "A thread initiated for processing $($csvimported.guid)"
                     Start-Job -ScriptBlock {requestImport $args[0] $args[1] $args[2] $args[3] $args[4] $args[5]} -InitializationScript $init -ArgumentList ($($csvimported.email), $($csvimported.guid), $($csvimported.computer),$collectionid,$username,$($list.fullname))
+                    $requested=$true
+                }
+                if($($csvimported.MACAddress) -and !$requested)
+                {
+                    writemsg "A thread initiated for processing $($csvimported.MACAddress)"
+                    Start-Job -ScriptBlock {requestImport $args[0] $args[1] $args[2] $args[3] $args[4] $args[5]} -InitializationScript $init -ArgumentList ($($csvimported.email), $($csvimported.MACAddress), $($csvimported.computer),$collectionid,$username,$($list.fullname))
                 }
                 
             }
@@ -307,7 +356,20 @@ if($null -ne $csvlist)
     Get-Job | Wait-Job  
     writemsg "All thread finished processing."
     $jobs=get-job | Receive-Job
+    $jobs = $jobs  | Group-Object -Property email
+    $jobs
+    foreach($j in $jobs)
+    {
+        $mailto=$($j.name)
+        $name=$($j.name).split('.')[0]
+        $table=$($j.group) | Select-Object computername,resourceid,uniqueid,collectionid,collectionname,status,message,comment | ConvertTo-Html -Head $test
+        $table=[Net.WebUtility]::Htmldecode($table)
+        $table
+        sendmail $mailto $name $table
+
+    }
     writemsg $jobs
+     Get-Job | remove-Job  
     $filelist = $jobs | Select-Object -Unique
     writemsg "List of unique .CSV files entry."
     $filelist | ForEach-Object { writemsg $_}
@@ -316,7 +378,7 @@ if($null -ne $csvlist)
         if($filelist -notcontains $($list.fullname))
         {
             writemsg "Moving $($list.fullname) to '$sharedpath\_backup\_csv'"
-            Move-Item $($list.fullname) -Destination "$sharedpath\_backup\_csv"
+            Move-Item $($list.fullname) -Destination "$sharedpath\_backup\_csv" -force
             writemsg "------------------------------------------------------------------------------------------------------------------------------------"
         }
     }
